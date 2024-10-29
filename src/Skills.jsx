9adca807726f6179c06b7d3a2c5b3d9376775ed7
phittayanan <phittayanan9@gmail.com>
2024-10-29 18:43:08 +0700

    update fix image is not working

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import './Skills.css';
 import { FaFlask, FaLaptopCode, FaRobot } from 'react-icons/fa';
+import researchImage from './assets/dow-61864548498-rd-shortmast.avif';
+import softwareImage from './assets/alberta-software-engineering-4x2.png';
+import aiImage from './assets/0276.jpg_wh300.jpg-e1652677128201.webp';
 
 const Skills = () => {
     const skills = [
         {
             title: "Researcher and Developer",
             description: "Conducts research and develops innovative solutions across various domains, applying scientific methods and technical skills to drive advancements and create impactful results.",
-            icon: <FaFlask size={40} color="#FFD700" />, // Gold icon color
-            image: "src/assets/dow-61864548498-rd-shortmast.avif" // Updated path for consistency
+            icon: <FaFlask size={40} color="#FFD700" />,
+            image: researchImage
         },
         {
             title: "Software Engineer",
             description: "Develops and maintains software applications by applying engineering principles to solve complex technical issues, enhancing functionality and user experience.",
             icon: <FaLaptopCode size={40} color="#FFD700" />,
-            image: "src/assets/alberta-software-engineering-4x2.png"
+            image: softwareImage
         },
         {
             title: "AI Engineer",
             description: "Creates and implements sophisticated AI systems, utilizing machine learning models and algorithms to automate tasks and improve decision-making processes effectively.",
             icon: <FaRobot size={40} color="#FFD700" />,
-            image: "src/assets/0276.jpg_wh300.jpg-e1652677128201.webp"
+            image: aiImage
         }
     ];
 
